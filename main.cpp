d339f23967324a21d346bf4bfaf5e235a2e24470
Karl Woditsch <k.woditsch@gmail.com>
2013-03-05 17:32:25 +0100

    netbeans project initialisation

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Karl
+ *
+ * Created on 5 mars 2013, 17:30
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
