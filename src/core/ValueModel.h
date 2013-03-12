57a6e16c9c36f448aa28a64e8e8f64ce6b507f99
Karl Woditsch <k.woditsch@gmail.com>
2013-03-12 09:29:20 +0100

    VM implement

--- src/core/ValueModel.h
+++ src/core/ValueModel.h
@@ -8,7 +8,45 @@
 #ifndef VALUEMODEL_H
 #define	VALUEMODEL_H
 
+#include <iostream>
 
+namespace core{
+    template<class T>
+    class ValueModel : public Expression{
+    private:
+        T _value;
+    public:
+        ValueModel(T value);
+        ValueModel(const ValueModel<T>& o);
+        virtual ~ValueModel();
+        void setValue(T value);
+        virtual T evaluate() const;
+    };
+    
+    template<class T>
+    ValueModel<T>::ValueModel(T value)
+    :_value(value){
+    }
+
+    template<class T>
+    ValueModel<T>::ValueModel(const ValueModel<T>& o)
+    :_value(o._value){
+    }
+
+    template<class T>
+    ValueModel<T>::~ValueModel(){
+    }
+    
+    template<class T>
+    void ValueModel<T>::setValue(T value){
+        _value = value;
+    }
+    
+    template<class T>
+    T ValueModel<T>::evaluate() const{
+        return _value;
+    }
+}
 
 #endif	/* VALUEMODEL_H */
 
