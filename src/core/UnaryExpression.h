19d88fd48ab5b0017c23a917267d25663aab1f8e
Karl Woditsch <k.woditsch@gmail.com>
2013-03-12 08:26:12 +0100

    UnaryExpression implement

--- src/core/UnaryExpression.h
+++ src/core/UnaryExpression.h
@@ -8,7 +8,13 @@
 #ifndef UNARYEXPRESSION_H
 #define	UNARYEXPRESSION_H
 
-
+namespace core{
+    template<class T>
+    class UnaryExpression{
+    public:
+        T evaluate(Expression<T>* o) const = 0;
+    };
+}
 
 #endif	/* UNARYEXPRESSION_H */
 
