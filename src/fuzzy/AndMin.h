828b8435c11920c3ccfa030a77964545c6796187
Karl Woditsch <k.woditsch@gmail.com>
2013-03-13 11:24:27 +0100

    And operators implements

--- src/fuzzy/AndMin.h
+++ src/fuzzy/AndMin.h
@@ -8,7 +8,22 @@
 #ifndef ANDMIN_H
 #define	ANDMIN_H
 
+#include "And.h"
 
+namespace fuzzy{
+    template<class T>
+    class AndMin : public And{
+    public:
+        virtual T evaluate(core::Expression<T>* left, core::Expression<T>* right);
+    };
+    
+    template<class T>
+    T evaluate(core::Expression<T>* left, core::Expression<T>* right){
+        T lt = left->evaluate();
+        T rt = right->evaluate();
+        return (lt < rt)?lt:rt;
+    }
+}
 
 #endif	/* ANDMIN_H */
 
--- src/fuzzy/AndMult.h
+++ src/fuzzy/AndMult.h
@@ -8,7 +8,22 @@
 #ifndef ANDMULT_H
 #define	ANDMULT_H
 
+#include "And.h"
 
+namespace fuzzy{
+    template<class T>
+    class AndMult : public And{
+    public:
+        virtual T evaluate(core::Expression<T>* left, core::Expression<T>* right);
+    };
+    
+    template<class T>
+    T evaluate(core::Expression<T>* left, core::Expression<T>* right){
+        T lt = left->evaluate();
+        T rt = right->evaluate();
+        return lt * rt;
+    }
+}
 
 #endif	/* ANDMULT_H */
 
