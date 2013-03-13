5ac55a35ff2b40a060a3a4a645890b7f9d5df772
Karl Woditsch <k.woditsch@gmail.com>
2013-03-13 14:15:10 +0100

    IsTriangle implement

--- src/fuzzy/IsTriangle.h
+++ src/fuzzy/IsTriangle.h
@@ -14,16 +14,40 @@ namespace fuzzy{
     template<class T>
     class IsTriangle : public Is{
     private:
-        T _min;
-        T _max;
-        T _mid;
+        T* _min;
+        T* _mid;
+        T* _max;
     public:
+        IsTriangle();
+        IsTriangle(const IsTriangle<T>& o);
+        virtual ~IsTriangle();
         virtual T evaluate(core::Expression<T>* operand) const;
     };
     
     template<class T>
-     T evaluate(core::Expression<T>* operand) const{
-        
+    IsTriangle<T>::IsTriangle(T* min, T* mid, T* max)
+    :_min(min), _mid(mid), _max(max){
+    }
+    
+    template<class T>
+    IsTriangle<T>::IsTriangle(const IsTriangle<T>& o)
+    :_min(o._min), _mid(o._mid), _max(o._max){
+    }
+    
+    template<class T>
+    IsTriangle<T>::~IsTriangle(){
+    }
+    
+    template<class T>
+    T IsTriangle<T>::evaluate(core::Expression<T>* operand) const{
+        T val = operand->evaluate();
+        if(val > _min && val < _mid){
+            //TODO
+        }
+        if(val > _mid && val < _max){
+            //TODO
+        }
+        return 0;
     }
 }
 
