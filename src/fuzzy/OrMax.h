a255e149cff9879004194ce42b44b4e3ce3b3696
Karl Woditsch <k.woditsch@gmail.com>
2013-03-13 11:27:12 +0100

    Or operators implements

--- src/fuzzy/OrMax.h
+++ src/fuzzy/OrMax.h
@@ -8,7 +8,22 @@
 #ifndef ORMAX_H
 #define	ORMAX_H
 
+#include "Or.h"
 
+namespace fuzzy{
+    template<class T>
+    class OrMax : public Or{
+    public:
+        virtual T evaluate(core::Expression<T>* left, core::Expression<T>* right);
+    };
+    
+    template<class T>
+    T evaluate(core::Expression<T>* left, core::Expression<T>* right){
+        T lt = left->evaluate();
+        T rt = right->evaluate();
+        return (lt < rt)?rt:lt;
+    }
+}
 
 #endif	/* ORMAX_H */
 
--- src/fuzzy/OrPlus.h
+++ src/fuzzy/OrPlus.h
@@ -8,7 +8,23 @@
 #ifndef ORPLUS_H
 #define	ORPLUS_H
 
+#include "Or.h"
 
+namespace fuzzy{
+    template<class T>
+    class OrMax : public Or{
+    public:
+        virtual T evaluate(core::Expression<T>* left, core::Expression<T>* right);
+    };
+    
+    template<class T>
+    T evaluate(core::Expression<T>* left, core::Expression<T>* right){
+        T lt = left->evaluate();
+        T rt = right->evaluate();
+        T sum = lt + rt;
+        return (sum > 1)?1:sum;
+    }
+}
 
 #endif	/* ORPLUS_H */
 
