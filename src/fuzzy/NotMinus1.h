ddbf5897de354d41e4be252bbef228797d0aca0d
Karl Woditsch <k.woditsch@gmail.com>
2013-03-13 11:45:40 +0100

    NotMinus implement

--- src/fuzzy/NotMinus1.h
+++ src/fuzzy/NotMinus1.h
@@ -5,10 +5,23 @@
  * Created on 13 mars 2013, 11:01
  */
 
-#ifndef NORMINUS1_H
-#define	NORMINUS1_H
+#ifndef NOTMINUS1_H
+#define	NOTMINUS1_H
 
+#include "Not.h"
 
+namespace fuzzy{
+    template<class T>
+    class NotMinus1 : public Not{
+    public:
+        virtual T evaluate(core::Expression<T>* operand) const;
+    };
+    
+    template<class T>
+     T evaluate(core::Expression<T>* operand) const{
+        return 1 - operand->evaluate();
+    }
+}
 
-#endif	/* NORMINUS1_H */
+#endif	/* NOTMINUS1_H */
 
