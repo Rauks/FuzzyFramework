4a3a34ce51db8040f3331f3883da32484b957735
Karl Woditsch <k.woditsch@gmail.com>
2013-03-19 22:04:42 +0100

    new IsTrapese* files

---
+++ src/fuzzy/IsTrapeseLeft.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   IsTrapeseLeft.h
+ * Author: Karl
+ *
+ * Created on 19 mars 2013, 22:02
+ */
+
+#ifndef ISTRAPESELEFT_H
+#define	ISTRAPESELEFT_H
+
+
+
+#endif	/* ISTRAPESELEFT_H */
+
---
+++ src/fuzzy/IsTrapeseRight.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   IsTrapeseRight.h
+ * Author: Karl
+ *
+ * Created on 19 mars 2013, 22:01
+ */
+
+#ifndef ISTRAPESERIGHT_H
+#define	ISTRAPESERIGHT_H
+
+
+
+#endif	/* ISTRAPESERIGHT_H */
+
