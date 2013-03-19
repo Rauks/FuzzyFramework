6f551159448f01dc78858db0c19f4fab31e9a023
Karl Woditsch <k.woditsch@gmail.com>
2013-03-19 22:07:37 +0100

    IsTrapeseRight implement

--- src/fuzzy/IsTrapeseRight.h
+++ src/fuzzy/IsTrapeseRight.h
@@ -8,7 +8,48 @@
 #ifndef ISTRAPESERIGHT_H
 #define	ISTRAPESERIGHT_H
 
+#include "../core/Expression.h"
+#include "Is.h"
 
+namespace fuzzy{
+    template<class T>
+    class IsTrapeseRight : public Is<T>{
+    private:
+        T _min;
+        T _midMin;
+    public:
+        IsTrapeseRight(const T& min, const T& midMin);
+        IsTrapeseRight(const IsTrapeseRight<T>& o);
+        virtual ~IsTrapeseRight();
+        virtual T evaluate(core::Expression<T>* operand) const;
+    };
+    
+    template<class T>
+    IsTrapeseRight<T>::IsTrapeseRight(const T& min, const T& midMin)
+    :_min(min), _midMin(midMin){
+    }
+    
+    template<class T>
+    IsTrapeseRight<T>::IsTrapeseRight(const IsTrapeseRight<T>& o)
+    :_min(o._min), _midMin(o._midMin){
+    }
+    
+    template<class T>
+    IsTrapeseRight<T>::~IsTrapeseRight(){
+    }
+    
+    template<class T>
+    T IsTrapeseRight<T>::evaluate(core::Expression<T>* operand) const{
+        T val = operand->evaluate();
+        if(val > _min && val < _midMin){
+             return (val - _min) / (_midMin - _min);
+        }
+        if(val >= _midMin){
+             return 1;
+        }
+        return 0;
+    }
+}
 
 #endif	/* ISTRAPESERIGHT_H */
 
