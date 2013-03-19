ceeaed11b3b0a4f4262325114f2ea5852aee2318
Karl Woditsch <k.woditsch@gmail.com>
2013-03-19 22:10:54 +0100

    IsTrapeseLeft implement

--- src/fuzzy/IsTrapeseLeft.h
+++ src/fuzzy/IsTrapeseLeft.h
@@ -8,7 +8,48 @@
 #ifndef ISTRAPESELEFT_H
 #define	ISTRAPESELEFT_H
 
+#include "../core/Expression.h"
+#include "Is.h"
 
+namespace fuzzy{
+    template<class T>
+    class IsTrapeseLeft : public Is<T>{
+    private:
+        T _midMax;
+        T _max;
+    public:
+        IsTrapeseLeft(const T& midMax, const T& max);
+        IsTrapeseLeft(const IsTrapeseLeft<T>& o);
+        virtual ~IsTrapeseLeft();
+        virtual T evaluate(core::Expression<T>* operand) const;
+    };
+    
+    template<class T>
+    IsTrapeseLeft<T>::IsTrapeseLeft(const T& min, const T& midMin, const T& midMax, const T& max)
+    :_midMax(midMax), _max(max){
+    }
+    
+    template<class T>
+    IsTrapeseLeft<T>::IsTrapeseLeft(const IsTrapeseLeft<T>& o)
+    :_midMax(o._midMax), _max(o._max){
+    }
+    
+    template<class T>
+    IsTrapeseLeft<T>::~IsTrapeseLeft(){
+    }
+    
+    template<class T>
+    T IsTrapeseLeft<T>::evaluate(core::Expression<T>* operand) const{
+        T val = operand->evaluate();
+        if(val <= _midMax){
+             return 1;
+        }
+        if(val > _midMax && val < _max){
+            return (val - _max) / (_midMax - _max);
+        }
+        return 0;
+    }
+}
 
 #endif	/* ISTRAPESELEFT_H */
 
