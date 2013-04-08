e00ad99864bf49e683084ca03e868dd17b9a21f5
Karl Woditsch <k.woditsch@gmail.com>
2013-04-08 17:31:01 +0200

    SugenoConclusion implement

--- src/fuzzy/SugenoDefuzzConclusion.h
+++ src/fuzzy/SugenoDefuzzConclusion.h
@@ -8,7 +8,59 @@
 #ifndef SUGENODEFUZZCONCLUSION_H
 #define	SUGENODEFUZZCONCLUSION_H
 
+#include <stdexcept>
 
+#include <vector>
 
-#endif	/* SUGENODEFUZZCONDITION_H */
+namespace fuzzy{
+    template<class T>
+    class SugenoDefuzzConclusion : public core::NaryExpression<T>{
+    private:
+        typedef typename std::vector<core::Expression<T>*>::const_iterator const_iterator;
+        typedef typename std::vector<core::Expression<T>*>::iterator iterator;
+        const std::vector<T>* _coeff;
+    public:
+        SugenoDefuzzConclusion(std::vector<T>* coeff);
+        SugenoDefuzzConclusion(const SugenoDefuzzConclusion<T>& o);
+        virtual ~SugenoDefuzzConclusion();
+        virtual T evaluate(std::vector<core::Expression<T>*>* operands) const = 0;
+    };
+    
+    template<class T>
+    SugenoDefuzzConclusion<T>::SugenoDefuzzConclusion(std::vector<T>* coeff)
+    :_coeff(coeff){
+    }
+    
+    template<class T>
+    SugenoDefuzzConclusion<T>::SugenoDefuzzConclusion(const SugenoDefuzzConclusion<T>& o)
+    :_coeff(o._coeff){
+    }
+    
+    template<class T>
+    SugenoDefuzzConclusion<T>::SugenoDefuzzConclusion(){
+    }
+    
+    template<class T>
+    T SugenoDefuzzConclusion<T>::evaluate(std::vector<core::Expression<T>*>* operands) const{
+        T result = 0;
+        const_iterator coeffIt = _coeff->begin();
+        const_iterator opIt = operands->begin();
+        
+        while(coeffIt != _coeff->end() && opIt != operands->end()){
+            result += (*coeffIt) * (*opIt)->evaluate();
+            coeffIt++;
+            opIt++;
+        }
+        if(opIt != operands->end()){
+            throw std::length_error("Not enougth operands to match coeffs");
+        }
+        if(coeffIt == operands->end()){
+            throw std::length_error("Not enougth coeffs");
+        }
+        result += (*coeffIt);
+        return result;
+    }
+}
+
+#endif	/* SUGENODEFUZZCONCLUSION_H */
 
