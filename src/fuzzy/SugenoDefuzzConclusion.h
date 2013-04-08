60070b9bdde07511b75338f91593a70a0381d564
Karl Woditsch <k.woditsch@gmail.com>
2013-04-08 16:54:58 +0200

    Add sugeno defuzz files

---
+++ src/fuzzy/SugenoDefuzzConclusion.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   SugenoDefuzzCondition.h
+ * Author: Karl
+ *
+ * Created on 8 avril 2013, 16:51
+ */
+
+#ifndef SUGENODEFUZZCONCLUSION_H
+#define	SUGENODEFUZZCONCLUSION_H
+
+
+
+#endif	/* SUGENODEFUZZCONDITION_H */
+
