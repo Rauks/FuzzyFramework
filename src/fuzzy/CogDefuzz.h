7d841ea18b1a2079101165b5403d7500856fb9eb
Karl Woditsch <k.woditsch@gmail.com>
2013-04-08 09:05:55 +0200

    fix hold & add defuzz classes

---
+++ src/fuzzy/CogDefuzz.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   CogDefuzz.h
+ * Author: Karl
+ *
+ * Created on 8 avril 2013, 09:00
+ */
+
+#ifndef COGDEFUZZ_H
+#define	COGDEFUZZ_H
+
+
+
+#endif	/* COGDEFUZZ_H */
+
---
+++ src/fuzzy/MamdaniDefuzz.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   MamdaniDefuzz.h
+ * Author: Karl
+ *
+ * Created on 8 avril 2013, 08:59
+ */
+
+#ifndef MAMDANIDEFUZZ_H
+#define	MAMDANIDEFUZZ_H
+
+
+
+#endif	/* MAMDANIDEFUZZ_H */
+
