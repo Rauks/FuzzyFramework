a537b2f1cb5a86763013603fcbd29e4bb93867c5
Karl Woditsch <k.woditsch@gmail.com>
2013-05-13 10:27:36 +0200

    Add mamdani full test

--- main.cpp
+++ main.cpp
@@ -189,9 +189,86 @@ void sugenoTest(){
  * 
  */
 int main(int argc, char** argv) {
-    
     //mamdaniTest();
-    sugenoTest();
+    //sugenoTest();
+    
+    
+    try{
+        //operators
+        fuzzy::OrMax<float> opOr;
+        fuzzy::ThenMin<float> opThen;
+        fuzzy::AggMax<float> opAgg;
+            //mamdani
+            fuzzy::CogDefuzz<float> opDefuzz(0, 25, 0.1);
+            //sugeno
+            fuzzy::SugenoDefuzz<float> opSugeno;
+            vector<float> coefs;
+            coefs.push_back(1);
+            coefs.push_back(1);
+            coefs.push_back(1);
+            fuzzy::SugenoDefuzzConclusion<float> opConclusion = fuzzy::SugenoDefuzzConclusion<float>(&coefs);
+        //unused operators (for factory init only)
+        fuzzy::NotMinus1<float> opNot;
+        fuzzy::AndMin<float> opAnd;
+        
+        //fuzzy expession factory 
+        fuzzy::FuzzyFactory<float> f(&opAnd, &opOr, &opNot, &opThen, &opAgg, &opDefuzz, &opSugeno, &opConclusion);
+        
+        //membership function
+        fuzzy::IsGaussian<float> poor(0, 6);
+        fuzzy::IsGaussian<float> good(5, 6);
+        fuzzy::IsGaussian<float> excellent(10, 6);
+        
+        fuzzy::IsTrapeseLeft<float> rancid(1, 4);
+        fuzzy::IsTrapeseRight<float> delicious(6, 9);
+        
+        fuzzy::IsTriangle<float> cheap(0, 6, 12);
+        fuzzy::IsTriangle<float> average(7, 13, 19);
+        fuzzy::IsTriangle<float> generous(14, 20, 26);
+
+        //values
+        core::ValueModel<float> service(0); //0-10
+        core::ValueModel<float> food(0);  // 0-10
+        core::ValueModel<float> tips(0);  // 0-26
+        
+        //Mamdani system
+        core::Expression<float> *r = 
+            f.newAgg(
+                f.newAgg(
+                    f.newThen(
+                        f.newOr(
+                            f.newIs(&service, &poor), 
+                            f.newIs(&food, &rancid)
+                        ),
+                        f.newIs(&tips,&cheap)
+                    ),
+                    f.newThen(
+                        f.newIs(&service,&good),
+                        f.newIs(&tips,&average)
+                    )
+                ),
+                f.newThen(
+                    f.newOr(
+                        f.newIs(&service, &excellent), 
+                        f.newIs(&food, &delicious)
+                    ),
+                    f.newIs(&tips,&generous)
+                )
+        );
+        
+        //Mamdani system defuzzification
+        core::Expression<float> *system = f.newMamdaniDefuzz(&tips, r);
+        
+        //Apply values
+        service.setValue(3);
+        food.setValue(8);
+        
+        //Print result
+        cout << "Mamdani tip result : " << endl << system->evaluate();
+        
+    } catch(const std::exception& ex){
+        cout << ex.what();
+    }
     
     return 0;
 }
